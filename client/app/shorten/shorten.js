e4043f820a1c4023b43f04000a2a0a2bafa93f86
espiler <monkeysrawesome@gmail.com>
2014-11-24 19:59:52 -0800

    minor cleanup

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,30 +1,8 @@
 angular.module('shortly.shorten', [])
 // var linkController = require("../server/links/linkController.js")
 
-.controller('ShortenController', function ($scope, $location, Links, $http) {
+.controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   angular.extend($scope, Links);
 
-
-
-  // $scope.addLink = function(link) {
-  //   return $http({
-  //     method: 'POST',
-  //     url: '/api/links',
-  //     data: {url: link},
-  //   })
-  //   .then(function(response) {
-  //       console.log(response)''
-  //       console.log('*******************success******************');
-  //       $scope.data.links.push();
-  //     },
-  //     function(response) { // optional
-  //       console.log('*******************FAILED******************');
-  //     }
-  //   );
-  // };
-
-  // $scope.link = {};
-
-  // $scope.addLink();
 });
